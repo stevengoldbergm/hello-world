208a909f7379fbebcc565eee2b46d086a553cc30
SG_Demi <stevengoldbergm@gmail.com>
2023-07-22 20:41:48 -0500

    Minor Change

--- Program.cs
+++ Program.cs
@@ -22,7 +22,7 @@ namespace HelloWorld // Use the name of your actual application
           Console.WriteLine(myInt + resultString);
 
           string myCow = "cow";
-          string myCapitalizedCow = "Cows";
+          string myCapitalizedCow = "Cow";
 
           if (myCow == myCapitalizedCow)
           {
